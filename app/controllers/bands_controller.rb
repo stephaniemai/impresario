0562fda87d67cd991246eacc4c5f4af7a02d8c70
thierry baeumle <gutehu@gmail.com>
2018-11-27 14:57:07 +0100

    adding the controllers

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,13 @@
+class BandsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'bands/show'
+  get 'bands/create'
+  get 'bands/new'
+  get 'bands/index'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show'
+  get 'user/new'
+  get 'user/create'
+  get 'user/show'
+  get 'user/etdit'
+  get 'user/update'
+  get 'bookings/new'
+  get 'bookings/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
