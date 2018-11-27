201d20164e5624f25f72702a547042e880989e46
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-27 18:46:07 +0100

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,25 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @band = Band.find(params[:band_id])
+    @user = current_user.id
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.band = Band.find(params[:band_id])
+    raise
+    if @booking.save!
+      redirect_to band_booking_path(@booking)
+    else
+      redirect_to new_band_booking_path
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:location, :start_date, :end_date, :price, :band_id, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :bands, only: [:show, :create, :new, :index]
+  resources :bands, only: [:show, :create, :new, :index] do
+      resources :bookings, only: [:new, :create, :show, :index]
+  end
   devise_for :users
   root to: 'bands#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
