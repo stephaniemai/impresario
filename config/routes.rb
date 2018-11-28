0ddec59a90aafa9bf781477663204b80e8d29750
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-28 16:44:35 +0100

    links and views are working except the band creation form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :bands, only: [:show, :create, :new, :index] do
-      resources :bookings, only: [:new, :create, :show, :index]
+      resources :bookings, only: [:new, :create]
   end
   devise_for :users
+  resources :bookings, only: [:index, :show]
   root to: 'bands#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
