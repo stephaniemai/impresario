8222f1d28fde9347af6fbd357d335b2f4e3c4dd9
HugoBayle <h.bayle@hotmail.fr>
2018-11-28 17:04:44 +0100

    design home page + body background color + modif navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
       resources :bookings, only: [:new, :create, :show, :index]
   end
   devise_for :users
-  root to: 'bands#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
