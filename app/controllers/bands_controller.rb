81aba4122ccedb75754debfbed4b3eb64e388470
stephaniemai <stephaniemai.jacques@gmail.com>
2018-11-29 11:18:31 +0100

    image added to band model

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -26,6 +26,6 @@ class BandsController < ApplicationController
   private
 
   def band_params
-    params.require(:band).permit(:name, :city, :conditions, :price)
+    params.require(:band).permit(:city, :conditions, :image, :name, :price)
   end
 end
