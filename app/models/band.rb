bd45e979c23b7624b156a915ee746223e72804a9
thierry baeumle <gutehu@gmail.com>
2018-11-29 12:25:05 +0100

    un debut de barre de recherche

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,6 @@
 class Band < ApplicationRecord
   belongs_to :user
   belongs_to :genre
+  include PgSearch
+  multisearchable :against => [:city], :ignoring => [:accents], :ignoring => [:uppercase]
 end
