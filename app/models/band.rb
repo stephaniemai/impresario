7ef01518864c44f47947f72ccdab41d2d575a774
thierry baeumle <gutehu@gmail.com>
2018-11-29 16:14:03 +0100

    cvbn

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,7 @@
 class Band < ApplicationRecord
   belongs_to :user
   belongs_to :genre
+  include PgSearch
+  multisearchable :against => [:city]
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_104336) do
+ActiveRecord::Schema.define(version: 2018_11_29_101358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_104336) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "genre_id"
+    t.string "image"
     t.index ["genre_id"], name: "index_bands_on_genre_id"
     t.index ["user_id"], name: "index_bands_on_user_id"
   end
@@ -46,6 +47,15 @@ ActiveRecord::Schema.define(version: 2018_11_28_104336) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
